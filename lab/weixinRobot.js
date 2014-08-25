6aea143ef24f354141a651b6ee8b1a2e0cd9be7e
Bosn <bosn@outlook.com>
2014-08-25 19:44:07 +0800

    阿萨德发撒旦法斯蒂芬

--- lab/weixinRobot.js
+++ lab/weixinRobot.js
@@ -1,12 +1,5 @@
-var tmpl = ['嘿嘿', '哈哈', '[晕]', '[左太极]', 
-	'[右太极]', '[飞吻]', '在干吗？', '忙啥呢', '我想你'];
-var ev = document.createEvent('HTMLEvents');
-var btn = $('.chatSend')[0];
-ev.initEvent('click', true, true);
-var timer = 1;
-
+var tmpl = ['嘿嘿', '哈哈', '[晕]', '[左太极]', '[右太极]', '[飞吻]', '在干吗？', '忙啥呢', '我想你'];
 setInterval(function() {
 	$('#textInput').val(tmpl[Math.floor(tmpl.length * Math.random())]);
-	btn.click();
-	timer++;
+	$('.chatSend')[0].click();
 }, Math.round(Math.random() * 100000));
